38a377741540405f5e3e391a47438cccc14f3f63
Jeremy <jrbudnack@starkandwayne.com>
2016-10-31 23:24:35 -0400

    Add a separate .build directory to ensure we can build the previously built version of the walkthrough, then compare it with what was checked in as the end-product - this acts as protection for if someone manually changes aws.md for some reason

--- bin/build_walkthrough.py
+++ bin/build_walkthrough.py
@@ -21,30 +21,53 @@ def extract_keys(d,n):
             print "{0}{1}: {2}".format(n, k, v)
     return retval
 
+def build(buildPath, outputPath):
+    with io.open(join(buildPath, infra_name, "walkthrough.md"), 'r', encoding="utf-8") as walkthroughFile:
+        walkthroughText = walkthroughFile.read()
+    with io.open(join(buildPath, infra_name, "parameters.json"), 'r', encoding="utf-8") as walkthroughParamsFile:
+        walkthroughParams = json.load(walkthroughParamsFile)
+    print "Extracting Parameters..."
+    walkthroughParams = extract_keys(walkthroughParams,'')
+    print ""
+
+    snippetFiles = [f for f in listdir(buildPath) if isfile(join(buildPath, f))]
+
+    for snippetFileName in snippetFiles:
+        print "Merging in " + snippetFileName + "..."
+        snippetFile = io.open(join(buildPath, snippetFileName), 'r', encoding="utf-8")
+        snippetText = snippetFile.read()
+        walkthroughText = string.replace(walkthroughText, "(( insert_file " + snippetFileName + " ))", snippetText)
+
+    print ""
+    print "Merging in Parameters..."
+    for k,v in walkthroughParams.iteritems():
+        walkthroughText = string.replace(walkthroughText, "(( insert_parameter " + k + " ))", v)
+
+    outputFile = io.open(outputPath, 'w', encoding="utf-8")
+    outputFile.write(walkthroughText)
+
 walkthroughPath = cwd + "/walkthrough"
+buildPath = walkthroughPath + "/.build"
 
-with io.open(walkthroughPath+"/" + infra_name + "/walkthrough.md", 'r', encoding="utf-8") as walkthroughFile:
-    walkthroughText = walkthroughFile.read()
+# Prime the pump if the build directory doesn't exist
+if not os.path.exists(buildPath):
+    os.mkdir(buildPath)
+    os.system("cp " + walkthroughPath + "/*.md " + buildPath)
+    os.system("cp -R " + walkthroughPath + "/" + infra_name + " " + buildPath)
 
-with io.open(walkthroughPath+"/"+ infra_name + "/parameters.json", 'r', encoding="utf-8") as walkthroughParamsFile:
-    walkthroughParams = json.load(walkthroughParamsFile)
-print "Extracting Parameters..."
-walkthroughParams = extract_keys(walkthroughParams,'')
-print ""
-
-snippetFiles = [f for f in listdir(walkthroughPath) if isfile(join(walkthroughPath, f))]
-
-for snippetFileName in snippetFiles:
-    print "Merging in " + snippetFileName + "..."
-    snippetFile = io.open(join(walkthroughPath, snippetFileName), 'r', encoding="utf-8")
-    snippetText = snippetFile.read()
-    walkthroughText = string.replace(walkthroughText, "(( insert_file " + snippetFileName + " ))", snippetText)
-
-print ""
-print "Merging in Parameters..."
-for k,v in walkthroughParams.iteritems():
-    walkthroughText = string.replace(walkthroughText, "(( insert_parameter " + k + " ))", v)
-
-outputFile = io.open(cwd + "/" + infra_name + ".md", 'w', encoding="utf-8")
-outputFile.write(walkthroughText)
-print "\n File " + infra_name + ".md is generated. Well Done!"
+# First build, so we can ensure that the product wasn't changed before the source files
+print "BUILDING WALKTHROUGH FOR INITIAL COMPARISON..."
+build(buildPath, join(buildPath, infra_name + ".md.out"))
+with io.open(join(cwd, infra_name + ".md"), 'r', encoding="utf-8") as oldWalkthroughFile:
+    oldWalkthroughText = oldWalkthroughFile.read()
+with io.open(join(buildPath, infra_name + ".md.out"), 'r', encoding="utf-8") as newWalkthroughFile:
+    newWalkthroughText = newWalkthroughFile.read()
+if oldWalkthroughText != newWalkthroughText:
+    print "ERROR - This walkthrough may have previously been manually changed without changing the source files."
+else:
+    print ""
+    print "BUILDING FINAL WALKTHROUGH FILE..."
+    os.system("cp " + walkthroughPath + "/*.md " + buildPath)
+    os.system("cp -R " + walkthroughPath + "/" + infra_name + " " + buildPath)
+    build(buildPath, join(cwd, infra_name + ".md"))
+    print "\n File " + infra_name + ".md is generated. Well Done!"
